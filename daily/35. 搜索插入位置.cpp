bffe53c8ddd581e93b602a301a091fbcb1f4d476
learnin-tap <2232726197@qq.com>
2020-04-12 21:20:24 +0800

    判断大小直接返回结果，注意相等的情况

---
+++ daily/35. 搜索插入位置.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(target<=nums[0])
+            return 0;
+        if(target>nums[nums.size()-1])
+            return nums.size();
+        for(int i=0;i<nums.size();i++){
+            if(target>nums[i] && target<=nums[i+1])
+                return i+1;
+        }
+        return nums.size();
+    }
+};
