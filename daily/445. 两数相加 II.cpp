2b443a782c53eac544f15802358c1ab3db2d98b2
learnin-tap <2232726197@qq.com>
2020-04-20 02:49:19 +0800

    复用链表反转函数优化了，也可以用栈做这题，还可以把值先用字符串存起来，再转为链表返回最终结果

--- daily/445. 两数相加 II.cpp
+++ daily/445. 两数相加 II.cpp
@@ -1,51 +1,56 @@
 /**
  * Definition for singly-linked list.
- * public class ListNode {
+ * struct ListNode {
  *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null) return l2;
-        if (l2 == null) return l1;
-        ListNode node1 = reverse(l1);
-        ListNode node2 = reverse(l2);
-        int carry = 0;
-        ListNode newHead = new ListNode(0);
-        ListNode curr = newHead;
-        while (node1 != null || node2 != null){
-            int x = node1 != null ? node1.val : 0;
-            int y = node2 != null ? node2.val : 0;
-            int num = (x + y + carry) % 10;
-            ListNode node = new ListNode(num);
-            curr.next = node;
-            curr = node;
-            carry = (x + y + carry) / 10;
-            if (node1 != null) node1 = node1.next;
-            if (node2 != null) node2 = node2.next;
+public:
+// 链表头插法逆序
+    ListNode* reverse(ListNode *head){
+        ListNode *L=new ListNode,*p=L;
+        while(head)
+        {
+            ListNode *temp=head->next;
+            head->next=p->next;
+            p->next=head;
+            head=temp;
         }
-        // 最后加上 carry
-        if (carry > 0) curr.next = new ListNode(carry);
-        ListNode res = reverse(newHead.next);
-        return res;
+        return L->next;
     }
-    // 链表反转
-    public ListNode reverse(ListNode head){
-        if (head == null) return null;
-        ListNode pre = null;
-        while (head != null){
-            ListNode next = head.next;
-            head.next = pre;
-            pre = head;
-            head = next;
-        }
-        return pre;
-    }
-}
 
-作者：kelly2018
-链接：https://leetcode-cn.com/problems/add-two-numbers-ii/solution/lian-biao-fan-zhuan-bu-jie-zhu-zhan-by-tiantianliu/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode *p=reverse(l1),*q=reverse(l2);
+        ListNode *temp_p=p,*flag;
+        while(p && q)
+        {        
+            //将对应节点之和加到p上
+            p->val+=q->val;
+            flag=p;
+            if(p->val>=10 && p->next){
+                p->val%=10;
+                p->next->val+=1;
+            }
+            p=p->next;
+            q=q->next;
+        }
+        if(!p)
+            flag->next=q;   //flag是p的前一个节点
+        while(flag->next)
+        {
+            if(flag->val>=10){
+                flag->val%=10;
+                flag->next->val+=1;
+            }
+            flag=flag->next;
+        }
+        if(flag->val>=10){
+            flag->val%=10;
+            ListNode *node=new ListNode(1);
+            flag->next=node;
+        }
+        return reverse(temp_p);
+    }
+};
--- daily/445. 两数相加 II.js
+++ daily/445. 两数相加 II.js
@@ -10,96 +10,47 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+var reverse=function(head){
+    var L=new ListNode,p=L;
+    while(head)
+    {
+        var temp=head.next;
+        head.next=p.next;
+        p.next=head;
+        head=temp;
+    }
+    return L.next;
+}
+
 var addTwoNumbers = function(l1, l2) {
-   var p=l1,q=l2;
-    var l1_temp=new ListNode,l2_temp=new ListNode,aaa=new ListNode(1),x=l1_temp,y=l2_temp;
-    var sum_l1=0,sum_l2=0;
-    while(l1)
-    {
-        p=l1.next;
-        l1.next=x.next;
-        x.next=l1;
-        l1=p;
-        sum_l1++;
-    }
-    while(l2)
-    {
-        q=l2.next;
-        l2.next=y.next;
-        y.next=l2;
-        l2=q;
-        sum_l2++;
-    }
-    x=l1_temp.next,y=l2_temp.next;
-        if(sum_l1>=sum_l2){
-            while(y){
-                x.val+=y.val;
-                if(x.val>=10){
-                    x.val%=10;
-                    if(x.next)                    
-                        x.next.val+=1;                                        
-                    else
-                        x.next=aaa;
-                    
-                }
-                x=x.next;
-                y=y.next;
+        var p=reverse(l1),q=reverse(l2);
+        var temp_p=p,flag;
+        while(p && q)
+        {        
+            //将对应节点之和加到p上
+            p.val+=q.val;
+            flag=p;
+            if(p.val>=10 && p.next){
+                p.val%=10;
+                p.next.val+=1;
             }
-             while(x&&x.next)
-            {
-                if(x.val/10==1){
-                    x.val=x.val%10;
-                    x.next.val= x.next.val+1;
-                }
-                x=x.next;
-            }
-            if(x&&x.val>=10){
-                x.val%=10;
-                x.next=aaa;
-            }
-            var res=new ListNode,flag=res;
-            x=l1_temp.next;
-            while(x)
-            {
-                p=x.next;
-                x.next=flag.next;
-                flag.next=x;
-                x=p;
-            }
-            return res.next;
+            p=p.next;
+            q=q.next;
         }
-        else if(sum_l1<sum_l2){
-            while(x){
-                y.val+=x.val;
-                if(y.val>=10){
-                    y.val%=10;
-                    y.next.val+=1;
-                }
-                x=x.next;
-                y=y.next;
+        if(!p)
+            flag.next=q;   //flag是p的前一个节点
+        while(flag.next)
+        {
+            if(flag.val>=10){
+                flag.val%=10;
+                flag.next.val+=1;
             }
-            while(y&&y.next)
-            {
-                if(y.val/10==1){
-                    y.val=y.val%10;
-                    y.next.val= y.next.val+1;
-                }
-                y=y.next;
-            }
-            if(y&&y.val>=10){
-                y.val%=10;
-                y.next=aaa;
-            }
-            var res=new ListNode,flag=res;
-            y=l2_temp.next;
-            while(y)
-            {
-                p=y.next;
-                y.next=flag.next;
-                flag.next=y;
-                y=p;
-            }
-            return res.next;
+            flag=flag.next;
         }
-        return NULL;
+        if(flag.val>=10){
+            flag.val%=10;
+            var node=new ListNode(1);
+            flag.next=node;
+        }
+        return reverse(temp_p);
 };
