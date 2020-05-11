7ddb1ccd9e8522e2a7b2bb799fec0c4447616869
learnin-tap <2232726197@qq.com>
2020-05-11 17:10:19 +0800

    递归解法遍历左右子树

---
+++ daily/404. 左叶子之和.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    var sum=0;
+    if(!root)
+        return 0;
+    if(root.left && !root.left.left && !root.left.right)
+        sum+=root.left.val;
+    return sum+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+};
