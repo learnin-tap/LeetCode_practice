12ef2fefd47818a65c9d2f28a99c4ea09a79e162
learnin-tap <2232726197@qq.com>
2020-05-11 17:30:41 +0800

    递归求解最深层数

---
+++ daily/104. 二叉树的最大深度.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(root) {
+    if(!root)
+        return 0;
+    var left=maxDepth(root.left);
+    var right=maxDepth(root.right);
+    return 1+Math.max(left,right);
+};
