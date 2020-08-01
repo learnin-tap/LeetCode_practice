47eaa736328e43eedd88f3222f510dc4ba22bb05
learnin-tap <2232726197@qq.com>
2020-08-01 18:38:47 +0800

    很典型的动态规划习题，用foreach迭代，要注意临界条件

---
+++ daily/动态规划/动态规划easy/303. 区域和检索 - 数组不可变.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ */
+var NumArray = function(nums) {
+    this.arr = []
+    let sum = 0
+    nums.forEach((item)=>{
+        let newval = item+sum
+        this.arr.push(newval)
+        sum = newval
+    })    
+};
+
+/** 
+ * @param {number} i 
+ * @param {number} j
+ * @return {number}
+ */
+NumArray.prototype.sumRange = function(i, j) {
+    if(i===0)
+        return this.arr[j]
+    return this.arr[j]-this.arr[i-1]
+        
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
+ */
