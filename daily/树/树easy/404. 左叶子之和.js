32d9e23176a9bd8657c6391ac5543bba83fad54b
learnin-tap <2232726197@qq.com>
2020-09-13 11:41:55 +0800

    新增一种栈写法

--- daily/树/树easy/404. 左叶子之和.js
+++ daily/树/树easy/404. 左叶子之和.js
@@ -9,11 +9,29 @@
  * @param {TreeNode} root
  * @return {number}
  */
-var sumOfLeftLeaves = function(root) {
-    var sum=0;
+var sumOfLeftLeaves = function(root) {   
+    // let sum = 0
+    // if(!root)
+    //     return 0
+    // if(root.left && !root.left.left && !root.left.right)
+    //     sum+=root.left.val
+    // return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+
+
+    // 栈写法
     if(!root)
-        return 0;
-    if(root.left && !root.left.left && !root.left.right)
-        sum+=root.left.val;
-    return sum+sumOfLeftLeaves(root.left)+sumOfLeftLeaves(root.right);
+        return 0
+    let sum = 0, stack = []
+    stack.push([root, false])  //栈用于保存节点及判断该节点是否为左节点
+    while(stack.length) {
+        let [node, isLeft] = stack.pop()
+        if(isLeft && !node.left && !node.right) //找到左子节点
+            sum += node.val
+        if(node.left)
+            stack.push([node.left, true])
+        if(node.right)
+            stack.push([node.right, false])
+    }
+    return sum
+
 };
