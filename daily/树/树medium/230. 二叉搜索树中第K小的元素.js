dae5664cefb6a3cfeb7ff08edb493b10737e5c78
learnin-tap <2232726197@qq.com>
2020-09-14 13:39:21 +0800

    中序遍历可直接求解

---
+++ daily/树/树medium/230. 二叉搜索树中第K小的元素.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthSmallest = function(root, k) {
+    let res = [], stack = []
+    while(root || stack.length > 0) {
+        while(root) {
+            stack.push(root)
+            root = root.left
+        }
+        root = stack.pop()
+        res.push(root.val)
+        root = root.right
+    }
+    return res[k-1]
+};
