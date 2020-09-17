04a300890c16386bd5b702205b4aeccd5e2a9805
learnin-tap <2232726197@qq.com>
2020-09-17 15:38:02 +0800

    精简了之前写的递归代码

--- daily/树/树easy/104. 二叉树的最大深度.js
+++ daily/树/树easy/104. 二叉树的最大深度.js
@@ -10,9 +10,14 @@
  * @return {number}
  */
 var maxDepth = function(root) {
+    // if(!root)
+    //     return 0;
+    // var left=maxDepth(root.left);
+    // var right=maxDepth(root.right);
+    // return 1+Math.max(left,right);
+
+    // 精简版
     if(!root)
-        return 0;
-    var left=maxDepth(root.left);
-    var right=maxDepth(root.right);
-    return 1+Math.max(left,right);
+        return 0
+    return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
