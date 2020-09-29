f6067004f91056155e3f5612347694ed028ca8b3
learnin-tap <2232726197@qq.com>
2020-09-29 19:53:46 +0800

    增加一种递归写法

--- daily/剑指 Offer 32 - II. 从上到下打印二叉树 II.js
+++ daily/剑指 Offer 32 - II. 从上到下打印二叉树 II.js
@@ -10,19 +10,34 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
-    if(!root)
-        return []
-    let res = [], queue = [root], level = 0
-    while(queue.length) {
-        res[level] = []
-        let levelNum = queue.length
-        while(levelNum--) {
-            const front = queue.shift()
-            res[level].push(front.val)
-            if(front.left)  queue.push(front.left)
-            if(front.right) queue.push(front.right)
+    // 非递归写法
+    // if(!root)
+    //     return []
+    // let res = [], queue = [root], level = 0
+    // while(queue.length) {
+    //     res[level] = []
+    //     let levelNum = queue.length
+    //     while(levelNum--) {
+    //         const front = queue.shift()
+    //         res[level].push(front.val)
+    //         if(front.left)  queue.push(front.left)
+    //         if(front.right) queue.push(front.right)
+    //     }
+    //     level++
+    // }
+    // return res
+
+    // 递归写法
+    let res = []
+    let traversal = (root,level) => {
+        if(root) {
+            if(!res[level])
+                res[level] = []
+            res[level].push(root.val)
+            traversal(root.left, level+1)
+            traversal(root.right, level+1)
         }
-        level++
     }
+    traversal(root, 0)
     return res
 };
