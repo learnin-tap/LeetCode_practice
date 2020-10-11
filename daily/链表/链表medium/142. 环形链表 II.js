b37f0caea6c136e6a7810f2f11018860922d0199
learnin-tap <2232726197@qq.com>
2020-10-11 13:46:42 +0800

    新增简单写法，利用额外空间哈希表

--- daily/链表/链表medium/142. 环形链表 II.js
+++ daily/链表/链表medium/142. 环形链表 II.js
@@ -11,19 +11,31 @@
  * @return {ListNode}
  */
 var detectCycle = function(head) {
-    var fast=head,slow=head;
-    while(fast && fast.next){
-        fast=fast.next.next;
-        slow=slow.next;
-        if(fast==slow){
-            fast=head;
-            while(fast){
-                if(fast==slow)
-                    return fast;
-                fast=fast.next;
-                slow=slow.next;        
+    // // 哈希表写法
+    // let map = new Map(), cur = head
+    // while(cur) {
+    //     if(map.has(cur))
+    //         return cur
+    //     map.set(cur)
+    //     cur = cur.next
+    // }
+    // return null
+    
+    // 数学公式写法
+    let fast = head, slow = head
+    while(fast && fast.next) {
+        fast = fast.next.next   //快指针走两步
+        slow = slow.next        //慢指针走一步
+        if(fast === slow) {     // 涉及数学公式的计算
+            fast = head
+            while(fast) {
+                if(fast == slow) {
+                    return fast
+                }
+                fast = fast.next
+                slow = slow.next
             }
         }
     }
-    return null;
+    return null
 };
