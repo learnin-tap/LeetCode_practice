69dd8b8d169b2809d190dc400be4b3fb8a7872d5
learnin-tap <2232726197@qq.com>
2020-10-15 03:12:36 +0800

    动态规划写法，时间复杂度On

---
+++ daily/剑指 Offer 42. 连续子数组的最大和.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let max = nums[0], sum = 0
+    for(let i=0; i<nums.length; i++) {
+        if(sum>0)
+            sum += nums[i]
+        else
+            sum = nums[i]
+        max = Math.max(sum, max)
+    }
+    return max
+};
