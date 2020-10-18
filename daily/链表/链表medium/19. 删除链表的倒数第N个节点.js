baf4c3a7cfce9abc9488af11c06b1da612083515
learnin-tap <2232726197@qq.com>
2020-10-18 10:11:15 +0800

    快慢指针扫描一趟

--- daily/链表/链表medium/19. 删除链表的倒数第N个节点.js
+++ daily/链表/链表medium/19. 删除链表的倒数第N个节点.js
@@ -11,16 +11,15 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    var fast=head,slow=head,flag=0;
-    while(fast)
-    {
-        if(flag>=n+1)  //为了定位这个倒数节点的前一位，便于后面节点的删除
-            slow=slow.next;
-        fast=fast.next;
-        flag++;
+    let fast = head, slow = head, flag = 0
+    while(fast) {
+        if(flag>n)
+            slow = slow.next
+        fast = fast.next
+        flag++
     }
-    if(slow==head && flag==n)   //如果是倒数第一个节点就返回除头结点之外的链表元素
-        return head.next;
-    slow.next=slow.next.next;   //所谓的删除的这个元素
-    return head;
+    if(flag==n && slow==head)   //倒数第一个节点是头结点的情况
+        return head.next
+    slow.next = slow.next.next  //删除的这个节点
+    return head
 };
