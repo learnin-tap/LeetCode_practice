da3517f172b5413ca9d7848fc4ac806879654760
learnin-tap <2232726197@qq.com>
2020-10-19 12:41:41 +0800

    栈运用，利用三目运算符和字符串比较方法优化了代码

--- daily/栈/栈easy/844. 比较含退格的字符串.js
+++ daily/栈/栈easy/844. 比较含退格的字符串.js
@@ -4,30 +4,10 @@
  * @return {boolean}
  */
 var backspaceCompare = function(S, T) {
-    var stack1=[],stack2=[];
-    for(let i=0;i<S.length;i++){
-        if(S[i]=='#')
-            if(S.length!=0) //不为空才能弹栈
-                stack1.pop();
-            else    //为空则不作任何处理
-                stack1=stack1;
-        else
-            stack1.push(S[i]);   
-    }
-
-    for(let i=0;i<T.length;i++){
-        if(T[i]=='#')
-            if(T.length!=0) //不为空才能弹栈
-                stack2.pop();
-            else    //为空则不作任何处理
-                stack2=stack2;
-        else
-           stack2.push(T[i]);
-    }
-    if(stack1.length!=stack2.length)    //栈的大小不等则不相等
-        return false;
-    for(let i=0;i<stack1.length;i++)
-        if(stack1[i]!=stack2[i])    //栈中任一元素不相等则不相等
-            return false;
-    return true;
+    let stack1 = [], stack2 = []
+    for(let item of S)
+        item == '#' ? stack1.pop() : stack1.push(item)
+    for(let item of T)
+        item == '#' ? stack2.pop() : stack2.push(item)
+    return stack1.join('') == stack2.join('')
 };
