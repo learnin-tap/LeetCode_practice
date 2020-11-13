67af6f4ad9911947c387364ff15d01a9c6636efb
learnin-tap <2232726197@qq.com>
2020-11-13 23:57:26 +0800

    用单调栈优化成了O(n)的时间复杂度

--- daily/栈/栈medium/739. 每日温度.js
+++ daily/栈/栈medium/739. 每日温度.js
@@ -3,15 +3,30 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(T) {
+    // 暴力解法
+    // const res = new Array(T.length).fill(0)
+    // for(let i=0; i<T.length; i++) {
+    //     let flag = T[i]
+    //     for(let j=i+1; j<T.length; j++) {
+    //         if(flag<T[j]) {
+    //             res[i] = j-i
+    //             break
+    //         }
+    //     }
+    // }
+    // return res
+
+    // 单调栈
     const res = new Array(T.length).fill(0)
-    for(let i=0; i<T.length; i++) {
-        let flag = T[i]
-        for(let j=i+1; j<T.length; j++) {
-            if(flag<T[j]) {
-                res[i] = j-i
-                break
-            }
+    const stack = []
+    for(let i=T.length-1; i>=0; i--) {
+        while(stack.length && T[i]>T[stack[stack.length-1]]) {
+            stack.pop()
         }
+        if(stack.length) {
+            res[i] = stack[stack.length-1] - i
+        }
+        stack.push(i)
     }
     return res
 };
