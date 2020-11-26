d5dd9c66c1a5accf68dc163512a19e1788d3cfe2
learnin-tap <2232726197@qq.com>
2020-11-27 02:39:25 +0800

    增加一种比较繁琐(内各种判断的方法）不推荐但mark一下，给自己长记性

--- daily/链表/链表medium/2. 两数相加.js
+++ daily/链表/链表medium/2. 两数相加.js
@@ -1,8 +1,8 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
@@ -11,25 +11,68 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    let p=l1,q=l2;
-    while(p.next && q.next){
-        p.val+=q.val;
-        p=p.next;
-        q=q.next;
+    // 方法1：在迭代中计算，这种方法略繁琐，很多情况需要考虑到，不推荐
+    // let p = l1, q = l2, flag = 0
+    // while(p && p.next && q && q.next) {
+    //     p.val = p.val + q.val + flag
+    //     if(p.val>=10) {
+    //         flag = 1
+    //         p.val %= 10
+    //     }
+    //     else
+    //         flag = 0
+    //     p = p.next
+    //     q = q.next
+    // }
+    // const newList = new ListNode(1)
+    // let temp = 0   //记录进位
+    // p.val = p.val + q.val + flag
+    // if(!p.next && !q.next && p.val>=10) {    //都无后续的情况
+    //     p.val %= 10
+    //     p.next = newList
+    // }
+    // if(!p.next && q.next)   // p 后续无值但 q 有，把 q 的接过来
+    //     p.next = q.next
+    // while(p && p.next) {    //正常迭代
+    //     p.val+=temp
+    //     if(p.val>=10) {
+    //         p.val %= 10
+    //         temp = 1
+    //     } else
+    //         temp = 0
+    //     p = p.next
+    // }
+    // if(temp==1) {
+    //     p.val += temp
+    //     if(p.val>=10) {
+    //         p.val %= 10
+    //         p.next = newList
+    //     }
+    // }
+    // return l1
+
+
+    // 方法2：先累加完，再重新计算进位等情况，比较简单的处理方法，很稳
+    let p = l1, q = l2
+    while(p.next && q.next) {
+        p.val += q.val
+        p = p.next
+        q = q.next
     }
-    p.val+=q.val;
-    if(!p.next)
-        p.next=q.next;
-    var list=new ListNode(1),flag=l1;
-    while(flag){
-        if(flag.val>=10){
-            flag.val%=10;
-            if(flag.next)
-                flag.next.val+=1;
+    p.val += q.val
+    if(!p.next) //不管q有没有后续都接过来，即使没有也不影响
+        p.next = q.next
+    const newList = new ListNode(1)
+    p = l1  //重头再遍历
+    while(p) {
+        if(p.val>=10) {
+            p.val %= 10
+            if(p.next)
+                p.next.val += 1
             else
-                flag.next=list;
+                p.next = newList
         }
-        flag=flag.next;
+        p = p.next
     }
-    return l1;
+    return l1
 };
