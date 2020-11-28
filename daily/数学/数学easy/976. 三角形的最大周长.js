f683046f2f7144f2e2b19e54f3a12a0af31293c3
learnin-tap <2232726197@qq.com>
2020-11-29 01:06:03 +0800

    排序+贪心解法，要想到这点才能解，用回溯都超时了

--- daily/数学/数学easy/976. 三角形的最大周长.js
+++ daily/数学/数学easy/976. 三角形的最大周长.js
@@ -3,17 +3,9 @@
  * @return {number}
  */
 var largestPerimeter = function(A) {
-    function compare(num1,num2){    //让sort排序从大到小
-        if(num1>num2)
-            return -1;
-        else if(num1<num2)
-            return 1;
-        else
-            return 0;
-    }
-    A.sort(compare);    //sort排序默认是从小到大的，需要加这个函数参数反过来
-    for(let i=0;i<A.length;i++)
-        if(A[i+1]+A[i+2]>A[i]) //只要较小的两边之和大于第三遍就满足三角形形成条件
-                return A[i+1]+A[i+2]+A[i];            
-    return 0;    //不满足输出0
+    A.sort((a,b)=>b-a)  //降序，从大到小
+    for(let i=0; i<A.length; i++)
+        if(A[i+1]+A[i+2]>A[i])
+            return A[i]+A[i+1]+A[i+2]   //满足最小的两个边大于最大的那个边就是最大周长的三条边
+    return 0
 };
