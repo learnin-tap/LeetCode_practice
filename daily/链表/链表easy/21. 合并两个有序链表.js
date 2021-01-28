1b94aae23e059b3b7b91825d1efb99e1f76c7769
左岸 <2232726197@qq.com>
2021-01-28 23:51:26 +0800

    递归+非递归两种链表合并方式

--- daily/链表/链表easy/21. 合并两个有序链表.js
+++ daily/链表/链表easy/21. 合并两个有序链表.js
@@ -1,8 +1,8 @@
 /**
  * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
  * }
  */
 /**
@@ -11,33 +11,32 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(l1, l2) {
-    var flag=new ListNode,p=flag;
-    while(l1 && l2)
-    {
-        if(l1.val>l2.val){
-            p.next=l2;
-            l2=l2.next;
-        }
-        else{
-            p.next=l1;
-            l1=l1.next;
-        }
-        p=p.next;
-    }
-    p.next=(l1==null)?l2:l1;
-    return flag.next;
-
-    //递归写法
+    // 递归写法O(n+m) O(n+m)时间、空间复杂度 n,m分别为两链表长度
     // if(l1==null)
-    //     return l2;
+    //     return l2
     // if(l2==null)
-    //     return l1;
-    // if(l1.val>l2.val){
-    //     l2.next=mergeTwoLists(l1,l2.next);
-    //     return l2;
+    //     return l1
+    // if(l1.val<l2.val) {
+    //     l1.next = mergeTwoLists(l1.next, l2)
+    //     return l1
+    // } else {
+    //     l2.next = mergeTwoLists(l1, l2.next)
+    //     return l2
     // }
-    // else{
-    //     l1.next=mergeTwoLists(l1.next,l2);
-    //     return l1;
-    // }
+
+    // 非递归写法，O(n+m)时间 O(1)空间
+    const newList = new ListNode()
+    let cur = newList
+    while(l1 && l2) {
+        if(l1.val<l2.val) {
+            cur.next = l1
+            l1 = l1.next
+        } else {
+            cur.next = l2
+            l2 = l2.next
+        }
+        cur = cur.next
+    }
+    cur.next = l1===null ? l2 : l1
+    return newList.next
 };
