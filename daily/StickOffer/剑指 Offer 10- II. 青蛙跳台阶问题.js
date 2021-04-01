ff0258b0fb76374e6bb6a49ea481e78addbe610f
Gary <fei98k@gmail.com>
2021-04-02 01:34:39 +0800

    '斐波拉契的思路求解'

--- daily/StickOffer/剑指 Offer 10- II. 青蛙跳台阶问题.js
+++ daily/StickOffer/剑指 Offer 10- II. 青蛙跳台阶问题.js
@@ -1,17 +1,14 @@
-/**
+﻿/**
  * @param {number} n
  * @return {number}
  */
-//和斐波拉契差不多的意思，我的思路是第n个台阶基于第n-1个和第n-2个上，即可以选择跳一步或者跳两步，然后就是暴力 递归
 var numWays = function(n) {
-    if(n==1 || n==0)
-        return 1
-    if(n==2)
-        return 2
-    let a = []
-    a[0]=1,a[1]=1,a[2]=2
-    for(let i=3;i<=n;i++){
-        a[i]=(a[i-1]+a[i-2])%1000000007
+    // 和斐波拉契同一个思路
+    let a = 1, b = 1, temp = 0
+    for(let i=2; i<=n; i++) {
+        temp = b
+        b = (a + b) % 1000000007
+        a = temp
     }
-    return a[n]
+    return b
 };
