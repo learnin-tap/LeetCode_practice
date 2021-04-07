79942db677572df7c9482931e821832bdd24b952
Gary <fei98k@gmail.com>
2021-04-07 23:07:57 +0800

    贪心写法，取余求解

---
+++ daily/StickOffer/剑指 Offer 14- II. 剪绳子 II.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var cuttingRope = function(n) {
+    // 贪心写法，尽量保持3的倍数就是答案
+    // if(n===2)  return 1
+    // if(n===3)  return 2
+    // let res = 1
+    // while(n > 4) {
+    //     res *= 3
+    //     res = res % 1000000007
+    //     n -= 3
+    // }
+    // return res * n % 1000000007
+
+    // 动态规划
+
+
+};
