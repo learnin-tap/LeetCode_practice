7cc2aa2f64772328a6d145f217377827f62a77c1
Gary <fei98k@gmail.com>
2021-04-24 23:24:07 +0800

    递归+递归两种写法，加上奇偶判断条件

--- daily/StickOffer/剑指 Offer 32 - III. 从上到下打印二叉树 III.js
+++ daily/StickOffer/剑指 Offer 32 - III. 从上到下打印二叉树 III.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Definition for a binary tree node.
  * function TreeNode(val) {
  *     this.val = val;
@@ -10,24 +10,40 @@
  * @return {number[][]}
  */
 var levelOrder = function(root) {
+    // 非递归写法
+    // if(!root)
+    //     return []
+    // const queue = [root]
+    // let flag = 0, res = []
+    // while(queue.length) {
+    //     flag++
+    //     const len = queue.length, arr = []
+    //     for(let i=0; i<len; i++) {
+    //         node = queue.shift()
+    //         arr.push(node.val)
+    //         if(node.left)  queue.push(node.left)
+    //         if(node.right) queue.push(node.right)
+    //     }
+    //     flag % 2 == 1 && res.push(arr)
+    //     flag % 2 == 0 && res.push(arr.reverse())
+    // }
+    // return res
+
+    // 递归写法
     if(!root)
         return []
-    const res = [], queue = [root]
-    let sum = 0 //记录层数
-    while(queue.length) {
-        const temp = [], len = queue.length //temp数组存放遍历时每层的所有节点
-        sum++
-        for(let i=0; i<len; i++) {  //len即为每层节点的总数
-            const node = queue.shift()
-            temp.push(node.val)
-            node.left && queue.push(node.left)
-            node.right && queue.push(node.right)
+    const res = []
+    const reversal = (root, level) => {
+        if(root) {
+            if(!res[level])
+                res[level] = []
+            // 奇偶异类
+            level%2==0 && res[level].push(root.val)
+            level%2==1 && res[level].unshift(root.val)
+            reversal(root.left, level+1)
+            reversal(root.right, level+1)
         }
-        // 交叉存入结果中
-        if(sum%2==0)
-            res.push(temp.reverse())
-        else
-            res.push(temp)
     }
+    reversal(root, 0)
     return res
 };
