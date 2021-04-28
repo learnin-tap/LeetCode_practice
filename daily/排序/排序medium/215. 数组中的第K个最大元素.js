b12adbf0ddd4b2647b8648160dc9086df4b8d63d
Gary <fei98k@gmail.com>
2021-04-29 02:57:52 +0800

    利用快速排序求解

--- daily/排序/排序medium/215. 数组中的第K个最大元素.js
+++ daily/排序/排序medium/215. 数组中的第K个最大元素.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
@@ -23,6 +23,33 @@ var findKthLargest = function(nums, k) {
     //     }
     // }
     // return nums[nums.length-k]
+	
+    // 内置排序
+    //return nums.sort((a,b)=>b-a)[k-1]
 
-    return nums.sort((a,b)=>b-a)[k-1]
+    // 手写快排
+    const quickSort = (arr, left, right) => {
+        if(!arr || arr.length==0)  return
+        if(left>right)  return
+        const key = arr[left]
+        let l = left, r = right
+        while(l!=r) {
+            // 相等情况必须考虑进来且l<r这个条件不能少
+            while(arr[r]<=key && l<r)
+                r--
+            while(arr[l]>=key && l<r)
+                l++
+            if(l<r) {
+                [arr[l], arr[r]] = [arr[r], arr[l]]
+            }
+        }
+        // 交换位置
+        arr[left] = arr[l]
+        arr[l] = key
+        quickSort(arr,left, l-1)
+        quickSort(arr, l+1, right)
+    }
+    quickSort(nums, 0, nums.length-1)
+    return nums[k-1]
+
 };
