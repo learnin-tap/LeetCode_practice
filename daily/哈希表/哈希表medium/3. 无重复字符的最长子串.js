c94670f69eb9ee6f9d70077febc8d250bf4bf5f0
Gary <fei98k@gmail.com>
2021-08-06 23:14:16 +0800

    利用哈希表和flag记录

--- daily/哈希表/哈希表medium/3. 无重复字符的最长子串.js
+++ daily/哈希表/哈希表medium/3. 无重复字符的最长子串.js
@@ -1,14 +1,15 @@
-/**
+﻿/**
  * @param {string} s
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
     let map = new Map()
-    let flag = 0, max = 0   // flag 为无重复子串起始位置
+    let max = 0, flag = 0 //flag为子串第一次出现的位置
     for(let i=0; i<s.length; i++) {
-        if(map.has(s[i]))
+        if(map.has(s[i])) {
             flag = Math.max(flag, map.get(s[i])+1)
-        max = Math.max(max, i-flag+1)
+        }
+        max = Math.max(max, i-flag+1) // max最小至少要为1，因为字符串长度为至少是1
         map.set(s[i], i)
     }
     return max
