badbe4f9fd3a2c34d3175118e3018ce0512ba68b
Gary <fei98k@gmail.com>
2021-08-09 02:38:44 +0800

    整理三种二叉树展开成链表的方法

--- daily/树/树medium/114. 二叉树展开为链表.js
+++ daily/树/树medium/114. 二叉树展开为链表.js
@@ -1,4 +1,4 @@
-/**
+﻿/**
  * Definition for a binary tree node.
  * function TreeNode(val, left, right) {
  *     this.val = (val===undefined ? 0 : val)
@@ -9,37 +9,40 @@
 /**
  * @param {TreeNode} root
  * @return {void} Do not return anything, modify root in-place instead.
- */
+ */  
 var flatten = function(root) {
-    // 递归写法1
-    // if(!root)   //退出条件
-    //     return null
-    // flatten(root.left)  //递归左孩子
-    // flatten(root.right) //递归右孩子
-    // let temp = root.right   //先把右孩子存起来
-    // root.right = root.left  //然后把右孩子换成左孩子
-    // root.left = null    //左孩子要置空
-    // while(root.right)   //把原来的左孩子都插到右边来
+    // 递归写法
+    // if(!root)  return null // 退出条件
+    // flatten(root.left)  // 递归左孩子
+    // flatten(root.right) // 递归右孩子
+    // let temp = root.right   // 把左孩子存起来
+    // root.right = root.left  // 把右孩子换成左孩子
+    // root.left = null    // 左孩子置空
+    // while(root.right)   // 一直找到最后一个右节点
     //     root = root.right
-    // root.right = temp   //把真正的右孩子接在原左孩子的右边
+    // root.right = temp// 通过最后一个节点把之前保存的右子树接上去
     
-    // 递归写法2
-    // let pre = null
-    // const dfs = node => {
-    //     if(!node)
-    //         return null
-    //     dfs(node.right)
-    //     dfs(node.left)
-    //     node.right = pre
-    //     node.left = null    //左孩子置空
-    //     pre = node
+    // 非递归写法1 拿到先序遍历结果后拆解子树
+    // if(!root)  return null
+    // const stack = [], res = []
+    // while(root || stack.length) {
+    //     while(root) {
+    //         stack.push(root)
+    //         res.push(root)
+    //         root = root.left
+    //     }
+    //     root = stack.pop()
+    //     root = root.right
     // }
-    // dfs(root)
-    // // return root  //原地修改，所以其实不用返回
+    // // res为拿到的先序遍历数组
+    // for(let i=1; i<res.length; i++) {
+    //     const prev = res[i-1], cur = res[i]
+    //     prev.left = null
+    //     prev.right = cur
+    // }
 
-    // 非递归写法
-    if(root==null)
-        return null
+    // 非递归写法2 在先序遍历的过程中拆借子树
+    if(!root) return null
     const stack = []
     while(root || stack.length) {
         while(root) {
@@ -48,15 +51,13 @@ var flatten = function(root) {
         }
         if(stack.length) {
             let node = stack.pop()
-            root = node.right   //root需要指向右孩子，因为右孩子如果有左孩子就要进行上面的while循环
-            let temp = node.right   //将右孩子存起来
-            node.right = node.left  //然后把右孩子换成左孩子
-            node.left = null  //左孩子要置空
-            while(node.right)   //把原来的左孩子都插到右边来
+            root = node.right// root需要指向右孩子，右孩子的左孩子需要进行上面的while循环
+            const temp = node.right //保存右子树
+            node.right = node.left
+            node.left = null
+            while(node.right)// 一直找到最后一个右节点
                 node = node.right
-            node.right = temp   //把真正的右孩子接在原左孩子的右边  
+            node.right = temp//通过最后一个右孩子把之前保存的右子树接上
         }
     }
-    // return root //原地修改，所以不用return
-
 };
