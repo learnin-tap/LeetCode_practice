1e6dccb12ced282b4875510180e1e968aaf738b2
Gary <fei98k@gmail.com>
2021-09-03 01:15:48 +0800

    遍历过程中找到左右最高柱子，短板效应求累积值

--- daily/双指针/双指针hard/42. 接雨水.js
+++ daily/双指针/双指针hard/42. 接雨水.js
@@ -1,21 +1,21 @@
-/**
+﻿/**
  * @param {number[]} height
  * @return {number}
  */
 var trap = function(height) {
-    var sum=0;
-     // 第一列和最后一列不会积水
-    for(var i=1;i<height.length-1;i++){
-        var max_left=0;
-        for(var j=0;j<i;j++)//求左边最高的柱子 
-            if(max_left<height[j])
-                max_left=height[j];
-        var max_right=0;
-        for(var j=i+1;j<height.length;j++)//右边最高的柱子
-            if(max_right<height[j])
-                max_right=height[j];
-        if(Math.min(max_left,max_right)>height[i])//短板效应，然后比当前列高才能积水
-        sum+=Math.min(max_left,max_right)-height[i];
+    let sum = 0
+    // 第一列和最后一列不会积水
+    for(let i=1; i<height.length-1; i++) {
+        let max_left = 0    // 找到左边最高柱子
+        for(let j=0; j<i; j++)
+            if(height[j] > max_left)
+                max_left = height[j]
+        let max_right = 0   // 找到右边最高柱子
+        for(let j=i+1; j<height.length; j++)
+            if(height[j] > max_right)
+                max_right = height[j]
+        if(Math.min(max_left, max_right) > height[i])   // 短板效应
+            sum += Math.min(max_left, max_right) - height[i]
     }
-    return sum;
+    return sum
 };
