871283bde25f036d8e07a05e98a754523d53834b
Gary <fei98k@gmail.com>
2021-09-02 19:34:18 +0800

    双指针解法，不断收窄比较面积

--- daily/数组/数组medium/11. 盛最多水的容器.js
+++ daily/数组/数组medium/11. 盛最多水的容器.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxArea = function(height) {
-    var left=0,right=height.length-1,max=0;
-    while(left<right)
-    {
-        if((right-left)*Math.min(height[left],height[right])>max)
-			max=(right-left)*Math.min(height[left],height[right]);
-		else
-			height[left]<height[right]?left++:right--;	
+    // 双指针解法，两边不断收窄比较最大面积
+    let left = 0, right = height.length-1, max = 0
+    while(left < right) {
+        if((right-left) * Math.min(height[left], height[right]) > max ) {
+            max = (right-left) * Math.min(height[left], height[right])
+        } else {
+            height[left] < height[right] ? left++ : right--
+        }
     }
-    return max;
+    return max
 };
